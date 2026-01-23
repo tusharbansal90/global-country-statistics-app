94185a8f31849df3d2c096635f285a34beedc638
TUSHAR BANSAL <tushar128022@gmail.com>
2026-01-23 18:15:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,6 +24,15 @@ def safe_float(val):
 st.title("🌍 Global Country Statistics Explorer")
 st.write("Explore, compare, and analyze key country statistics.")
 
+# -------------------- COUNTRY SELECTOR --------------------
+country = st.sidebar.selectbox(
+    "Select a Country",
+    sorted(df["Country"].unique())
+)
+
+selected = df[df["Country"] == country].iloc[0]
+
+
 # ---------------- SIDEBAR ----------------
 st.sidebar.header("🔎 Select Countries")
 
